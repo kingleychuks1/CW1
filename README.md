f7b4f6710f72e85560fd29f5c5d71ecf40b5ecf1
kingleychuks1 <kingleychuks@gmail.com>
2023-10-28 19:03:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CST3145-individual-CW1.github.io
+A repository for my first Individual Course work for CST3145. 
---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+	{
+        id: 1,
+        subject: "Math",
+        location: "Manchester",
+        price : 350,
+        image: "images/maths.jpeg",
+        availablespace: 5,
+        rating: 4
+    },
+    {
+        id: 2,
+        subject: "English",
+        location: "Edinburgh",
+        price : 300,
+        image: "images/english.jpeg",
+        availablespace: 5,
+        rating: 5
+    },
+    {
+        id: 3,
+        subject: "Physics",
+        location: "Liverpool",
+        price : 250,
+        image: "images/physics.jpeg",
+        availablespace: 5,
+        rating: 4  
+    },
+    {
+        id: 4,
+        subject: "Chemistry",
+        location: "Glasgow",
+        price : 200,
+        image: "images/chemistry.jpeg",
+        availablespace: 5,
+        rating: 3    
+    },
+    {
+        id: 5,
+        subject: "Biology",
+        location: "London",
+        price : 250,
+        image: "images/biology.jpeg",
+        availablespace: 5,
+        rating: 3    
+    },
+    {
+        id: 6,
+        subject: "Python",
+        location: "Stratford",
+        price : 300,
+        image: "images/python.jpeg",
+        availablespace: 5,
+        rating: 4    
+    },
+    {
+        id: 7,
+        subject: "Economics",
+        location: "Cardiff",
+        price : 100,
+        image: "images/economics.jpeg",
+        availablespace: 5,
+        rating: 1   
+    },
+    {
+        id: 8,
+        subject: "Music",
+        location: "Tottenham",
+        price : 150,
+        image: "images/music.jpeg",
+        availablespace: 5,
+        rating: 3 
+    },
+    {
+        id: 9,
+        subject: "Catering",
+        location: "Birmingham",
+        price : 100,
+        image: "images/catering.jpeg",
+        availablespace: 5,
+        rating: 2
+    },
+    {
+        id: 10,
+        subject: "Android",
+        location: "Glasgow",
+        price : 350,
+        image: "images/android.jpeg",
+        availablespace: 5,
+        rating: 5      
+    },
+]
